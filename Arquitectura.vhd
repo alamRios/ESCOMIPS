3025ec10cc70c81a4963b4b6aaa2feba10d117e5
alamRios <alam.rios@ymail.com>
2018-06-07 19:49:33 -0500

    Ensamblados

---
+++ Arquitectura.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:47:25 06/07/2018 
+-- Design Name: 
+-- Module Name:    Arquitectura - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Arquitectura is
+    Port ( clr,clk : in  STD_LOGIC;
+           datos,pc : out  STD_LOGIC_VECTOR (7 downto 0));
+end Arquitectura;
+
+architecture Behavioral of Arquitectura is
+
+begin
+
+
+end Behavioral;
+
---
+++ Ensamblado.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity Ensamblado is
+    Port ( clr,clr : in  STD_LOGIC;
+           datos : out  STD_LOGIC_VECTOR (7 downto 0));
+end Ensamblado;
+
+architecture Behavioral of Ensamblado is
+
+begin
+
+
+end Behavioral;
+
